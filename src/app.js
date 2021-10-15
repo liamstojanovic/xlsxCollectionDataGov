966c0d095a6a089a4c01e0c6abe6c2a5b1119f53
Liam Stojanovic <liam.stojanovic@gmail.com>
2021-10-15 12:49:01 -0400

    startApp

--- src/app.js
+++ src/app.js
@@ -1,2 +1,10 @@
 const network = require('./network')
+const dataClass = require('./data')
 
+async function startApp() {
+    const unparsedApiReturn = await network.retrieveResults('EXCEL')
+    const dataStore = new dataClass.Data(unparsedApiReturn)
+    console.log(dataStore.getXlsCount())
+}
+
+startApp()
