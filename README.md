7d965fe6b2648f6c84f9af19f311de08a84713a7
Liam Stojanovic <liam.stojanovic@gmail.com>
2021-10-15 17:31:14 -0400

    comments, readme clarity

--- README.md
+++ README.md
@@ -2,6 +2,8 @@ This is a collection of XLSX files extracted from data.gov, as well as a utility
 
 Network.js: Constructs API calls, fetches all possible results
 
-Data.js: A class to manage the data state
+Data.js: A class to manage the API data
+
+App.js: Logic for program. Change the input on line 5 to your desired search term. Default is 'EXCEL'
 
 https://docs.ckan.org/en/latest/api/index.html
--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const network = require('./network')
 const dataClass = require('./data')
 
 async function startApp() {
-    const unparsedApiReturn = await network.retrieveResults('EXCEL')
+    const unparsedApiReturn = await network.retrieveResults('EXCEL') // Change this to your desired search term
     const dataStore = new dataClass.Data(unparsedApiReturn)
     const rawUrls = dataStore.getXlsUrls()
     await network.downloadFile(rawUrls)
