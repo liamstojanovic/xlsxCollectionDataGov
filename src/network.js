3f96b57b781f57ef57c1a29e1b1424af7e388347
Liam Stojanovic <liam.stojanovic@gmail.com>
2021-10-15 12:07:43 -0400

    boilerplate code

--- src/network.js
+++ src/network.js
@@ -1,6 +1,9 @@
 const dataGovApi = require('./shared/dataGov')
 const axios = require('axios').default;
 
+/**
+ * Tests the catalog.data.gov api
+ */
 async function testApi () {
     try {
         var response = await axios.get(dataGovApi.baseUrl + dataGovApi.paths.status_show)
@@ -15,46 +18,50 @@ async function testApi () {
 
 }
 
-async function searchPackages (searchTerm) {
+/**
+ * 
+ * @param {string} searchTerm The search term
+ * @returns an array containing API response objects. The length of the array is equal to the requests made
+ */
+async function retrieveResults (searchTerm) {
     try {
-        var response = await axios.get(dataGovApi.baseUrl + dataGovApi.paths.package_search + `?q=${searchTerm}`)
-        var resultCount = await response.data.result.count
-        console.log(response.data.result)
+        var results = []
+        var requests = await generateApiRequests(searchTerm)
+        const totalRequests = requests.length
+        for await (const [index, value] of requests.entries()) {
+            console.log(`Fetching remote resource from catalog.data.gov, ${(index / totalRequests) * 100}% complete (${index}/${totalRequests}) `)
+            var response = await axios.get(value)
+            results.push(response)
+        }
+        console.log(`Fetched all remote resources, 100% complete (${totalRequests}/${totalRequests})`)
+        return results
     } catch(err) {
-        console.log('Error connecitng to data.gov API')
         console.log(err)
+        console.log('Error fetching URLs')
     }
 }
-
-async function retrieveUrlsFromPackages (searchTerm) {
-    try {
-        var start = 0
-        var response = await axios.get(dataGovApi.baseUrl + dataGovApi.paths.package_search + `?q=${searchTerm}` + `&rows=1000` + `&start=${start}`)
-        var resultCount = await response.data.result.count
-        console.log(response.data)
-    } catch(err) {
-
-    }
-}
-
-async function generateApiRequests(term) {
+/**
+ * 
+ * @param {string} term the search term to use
+ * @returns an array with api requests to make, in order to retrieve all possible results
+ */
+async function generateApiRequests(term) { 
     try {
         var startValues = []
-        var start = 0
         var init = await axios.get(dataGovApi.baseUrl + dataGovApi.paths.package_search + `?q=${term}`)
         var resultCount = await init.data.result.count
-        for (let i = 0; i < resultCount; i+= dataGovApi.MAXROWS) {
+        for (let i = 0; i < resultCount; i+= dataGovApi.MAXROWS) { // Creates an array of start values based on the resultCount response
             startValues.push(i)
         }
-        console.log(startValues)
+        // console.log(startValues)
         const apiRequests = startValues.map((element) => {
             return dataGovApi.baseUrl + dataGovApi.paths.package_search + `?q=${term}` + `&rows=${dataGovApi.MAXROWS}` + `&start=${element}`
         })
-        console.log(apiRequests)
+        return apiRequests
     } catch(err) {
         console.log(err)
         console.log('Error connecting to data.gov API')
     }
 }
 
-generateApiRequests('EXCEL')
+retrieveResults('EXCEL')
