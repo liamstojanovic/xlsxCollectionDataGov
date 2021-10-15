bf4c1551780f0392b8ec091bc2c6a132dbeb50e0
Liam Stojanovic <liam.stojanovic@gmail.com>
2021-10-15 10:35:04 -0400

    added function to generate api requests

--- src/network.js
+++ src/network.js
@@ -26,4 +26,35 @@ async function searchPackages (searchTerm) {
     }
 }
 
-searchPackages('EXCEL')
+async function retrieveUrlsFromPackages (searchTerm) {
+    try {
+        var start = 0
+        var response = await axios.get(dataGovApi.baseUrl + dataGovApi.paths.package_search + `?q=${searchTerm}` + `&rows=1000` + `&start=${start}`)
+        var resultCount = await response.data.result.count
+        console.log(response.data)
+    } catch(err) {
+
+    }
+}
+
+async function generateApiRequests(term) {
+    try {
+        var startValues = []
+        var start = 0
+        var init = await axios.get(dataGovApi.baseUrl + dataGovApi.paths.package_search + `?q=${term}`)
+        var resultCount = await init.data.result.count
+        for (let i = 0; i < resultCount; i+= dataGovApi.MAXROWS) {
+            startValues.push(i)
+        }
+        console.log(startValues)
+        const apiRequests = startValues.map((element) => {
+            return dataGovApi.baseUrl + dataGovApi.paths.package_search + `?q=${term}` + `&rows=${dataGovApi.MAXROWS}` + `&start=${element}`
+        })
+        console.log(apiRequests)
+    } catch(err) {
+        console.log(err)
+        console.log('Error connecting to data.gov API')
+    }
+}
+
+generateApiRequests('EXCEL')
